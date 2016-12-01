e934821c70aa297925fc9740582434d5fe0c7cf2
bert <btemme@hotmail.com>
2016-12-01 16:41:07 +0100

    trying to get connection to mongodb working

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var path = require('path');
+var mongodb = require('mongodb'); 
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+// try to get mongodb connection working...
+// with the other user/password
+var mongoDbUrl = 'mongodb://monguser:mongopw!@127.0.0.1:27017/Guardians';
+var mongoClient = mongodb.MongoClient;
+mongoClient.connect(mongoDbUrl, function(err, db) {
+    if(err){
+        // but got following error...
+        // error: MongoError: failed to connect to server [127.0.0.1:27017] on first connect
+        console.log("error connection to database. Is database started?");
+        console.log("error: " + err);
+    }
+    else{
+        console.log("Connected correctly to server");
+        dbObservations = db.collection('projects');
+    }
+});    
+
+
 app.use(express.static(path.join(__dirname, 'app')));
 var server = app.listen(port);
 module.exports = server;
